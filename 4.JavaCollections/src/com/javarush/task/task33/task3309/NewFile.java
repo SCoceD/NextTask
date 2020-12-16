97f88c4825d23d4e0b90a7b4e63a9ea8be4994cf
CoceD <scoced@gmail.com>
2020-12-16 23:18:43 +0100

    add new solution to Artem lesson

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/NewFile.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task33.task3309;
+
+//            '><><><<>>--><<--<<asdadasp;amv;s,dc'.a/>?>?>?>?>>>>>????>>-->>>><<--<<'
+//            >>--> || <--<<
+public class NewFile {
+    public static void main(String[] args) {
+        String str = "'><><><<>>--><<--<<asdadasp;amv;s,dc'.a/>?>?>?>?>>>>>????>>-->>>><<--<<'";
+        int positionIndex = 0;
+        int positionIndex1 = 0;
+        int count = 0;
+        while (str.indexOf(">>-->", positionIndex) != -1) {
+            if (str.indexOf(">>-->", positionIndex) != -1) {
+                positionIndex = str.indexOf(">>-->", positionIndex) + 1;
+                count++;
+            }
+        }
+        while (str.indexOf("<--<<", positionIndex1) != -1) {
+            if (str.indexOf("<--<<", positionIndex1) != -1) {
+                positionIndex1 = str.indexOf("<--<<", positionIndex1) + 1;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
